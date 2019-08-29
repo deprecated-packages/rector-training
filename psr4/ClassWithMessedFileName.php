87160af6444ad384af8d69fed972bb9ff0400c2b
Jan Mikeš <j.mikes@me.com>
2019-08-29 07:56:46 +0200

    Typed properties demo

--- psr4/ClassWithMessedFileName.php
+++
@@ -1,8 +0,0 @@
-<?php declare (strict_types=1);
-
-namespace Rector\RectorTraining\PSR4;
-
-final class ClassWithMessedNameDifferentToFileName
-{
-
-}
---
+++ src/TypeHints/Php74TypedProperties.php
@@ -0,0 +1,47 @@
+<?php declare (strict_types=1);
+
+namespace Rector\RectorTraining\TypeHints;
+
+final class Php74TypedProperties
+{
+	/**
+	 * @var MessedUpClass|null
+	 */
+	public $anotherClass = null;
+
+	/**
+	 * @var string
+	 */
+	public $string;
+
+	/**
+	 * @var string
+	 */
+	public $surname;
+
+	/**
+	 * @var bool
+	 */
+	public $name = 'not_a_bool';
+
+	/**
+	 * @var iterable
+	 */
+	public $iterable = [1, 2, 3];
+
+	public function run()
+	{
+		if ($this->string !== '') {
+			return $this->string;
+		}
+		return $this->surname;
+	}
+
+	public function anonymous()
+	{
+		$value = function($name) {
+			return $name . ', lets party!';
+		};
+		return $value('People');
+	}
+}
