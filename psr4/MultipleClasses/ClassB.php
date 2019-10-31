2800aeab39dee40ff6d6a30948dce61a3e8750e5
Tomas Votruba <tomas.vot@gmail.com>
2019-10-31 11:53:01 +0100

    wip

--- psr4/MultipleClasses/ClassB.php
+++ psr4/MultipleClasses/ClassB.php
@@ -2,10 +2,13 @@
 
 namespace Rector\RectorTraining\PSR4\MultipleClasses;
 
+<<<<<<< HEAD:psr4/MultipleClasses/MultipleClasses.php
 final class ClassA
 {
 }
 
+=======
+>>>>>>> wip:psr4/MultipleClasses/ClassB.php
 final class ClassB
 {
 }
